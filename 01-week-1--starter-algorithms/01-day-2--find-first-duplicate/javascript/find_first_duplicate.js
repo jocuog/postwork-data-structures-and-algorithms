f211bf0a5f68c376a5ed448811d06a0018a9f2e5
Johnathan <jocuog@gmail.com>
2022-07-04 11:03:47 -0600

    find first duplicate js completed

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -1,5 +1,13 @@
 function findFirstDuplicate(arr) {
   // type your code here
+
+  let elementsSet = new Set(); // This new Set object allows the storgae of each element from the given arrat as a unique value and check if it alreadt contains a value using Set.add() and Set.has(elementsSet).
+
+  for ( let i = 0; i < arr.length; i++) { // looping through the given array
+    if (elementsSet.has(arr[i])) return arr[i]; // check to see if the Set already contains the element that we are on currentlt on in the loop. if it exists return that value
+    elementsSet.add(arr[i]); // if the element is not in the Set add the element to the Set and move to the next element in the array
+  } 
+    return -1; // if no duplicates are found return -1
 }
 
 if (require.main === module) {
