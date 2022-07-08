529faf2d56b0ec48e81bf0e79811f903d473ddb2
Johnathan <jocuog@gmail.com>
2022-07-08 12:28:25 -0600

    recursive demo

--- 02-week-2--recursion/01-day-1--recursive-counting/ruby/recursive_count.rb
+++ 02-week-2--recursion/01-day-1--recursive-counting/ruby/recursive_count.rb
@@ -2,8 +2,21 @@ def recursive_count(num = 0)
   # type your code in here
 end
 
+
+def talk_to_myself(n)
+  return puts'done' if n <= 0.5
+
+  talk_to_myself(n/2)
+  'the sheep goes baah'
+end
+
+
+
+
 if __FILE__ == $PROGRAM_NAME
   recursive_count
+
+  puts "=>", talk_to_myself(4)
 end
 
 # OPTIONAL
