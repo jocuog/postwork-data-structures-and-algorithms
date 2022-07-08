e7fa50ab0501b3206b1e360621c015fc6a645bd5
Johnathan <jocuog@gmail.com>
2022-07-08 12:40:45 -0600

    first attempt recursive count solution

--- 02-week-2--recursion/01-day-1--recursive-counting/ruby/recursive_count.rb
+++ 02-week-2--recursion/01-day-1--recursive-counting/ruby/recursive_count.rb
@@ -1,5 +1,8 @@
 def recursive_count(num = 0)
   # type your code in here
+  return num if num > 0
+
+  recursive_count(num + 1)
 end
 
 
@@ -16,7 +19,10 @@ end
 if __FILE__ == $PROGRAM_NAME
   recursive_count
 
-  puts "=>", talk_to_myself(4)
+  puts "Expecting: '0,1,2,3,4,5,6,7,8,9'"
+  puts '=>', recursive_count(9)
+
+  # puts "=>", talk_to_myself(4)
 end
 
 # OPTIONAL
