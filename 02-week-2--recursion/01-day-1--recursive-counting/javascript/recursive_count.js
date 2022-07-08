1ea5035b185395d2eca3e7cf8b787a1852664626
Johnathan <jocuog@gmail.com>
2022-07-08 13:04:54 -0600

    javascript recursive solution

--- 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
+++ 02-week-2--recursion/01-day-1--recursive-counting/javascript/recursive_count.js
@@ -1,5 +1,10 @@
 function recursiveCount(num = 0) {
   // type your code here
+  if (num >= 10) {    // base case for ten frames in the stack
+    return
+    }
+    console.log(num)
+    recursiveCount(num + 1)   // call recursive method at the end of the function
 }
 
 if (require.main === module) {
