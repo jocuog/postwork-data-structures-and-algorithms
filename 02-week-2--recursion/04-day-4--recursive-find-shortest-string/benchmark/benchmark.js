5a9e79f13fe6a8139d59cb08c15aefaa90e3685a
Johnathan <jocuog@gmail.com>
2022-07-12 11:23:02 -0600

    ruby find shortest string recursive solution

--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/benchmark/benchmark.js
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/benchmark/benchmark.js
@@ -1,16 +1,22 @@
 // Paste your iterative and recursive solutions in this file
 // And then calculate their average run times to compare them.
 
+function findShortestStringRecursive(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  }
 
+  const result = findShortestStringRecursive(arr.slice(1));
 
-
+  return arr[0].length <= result.length ? arr[0] : result;
+}
 
 
 function benchmark(callback) {
   const startTime = Date.now();
 
   for (let i = 0; i < 1000; ++i) {
-    callback();
+    callback(findShortestStringRecursive(['flower', 'juniper', 'lily', 'dadelion']));
   }
 
   return (Date.now() - startTime) / 1000;
--- 02-week-2--recursion/04-day-4--recursive-find-shortest-string/ruby/find_shortest_string_recursive.rb
+++ 02-week-2--recursion/04-day-4--recursive-find-shortest-string/ruby/find_shortest_string_recursive.rb
@@ -1,5 +1,10 @@
 def find_shortest_string_recursive(arr)
   # type your code in here
+  return arr.first if arr.length == 1
+
+  result = find_shortest_string_recursive(arr[1..-1])
+
+  arr.first.length <= result.length ? result : result
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -15,6 +20,29 @@ if __FILE__ == $PROGRAM_NAME
 
   puts "Expecting: 'lily'"
   puts "=>", find_shortest_string_recursive(['flower', 'juniper', 'lily', 'dandelion'])
+
+  puts("Expecting: 'cat'");
+  puts("=>", find_shortest_string_recursive(['cat']))
+
+  puts
+
+  puts("Expecting: 'e'");
+  puts("=>", find_shortest_string_recursive(['e','tt','ada','dasfs','ff']))
+
+  puts
+
+  puts("Expecting: 'cat'");
+  puts("=>", find_shortest_string_recursive(['cat', 'lamb', 'elephant', 'ship']))
+
+  puts
+
+  puts("Expecting: 'ee'");
+  puts("=>", find_shortest_string_recursive(['ee', 'eeee','eeeeee','eeeeeee']))
+
+  puts
+
+  puts("Expecting: ''");
+  puts("=>", find_shortest_string_recursive(['','ds','adsf','ddda','as']))
 
   # Don't forget to add your own!
 end
