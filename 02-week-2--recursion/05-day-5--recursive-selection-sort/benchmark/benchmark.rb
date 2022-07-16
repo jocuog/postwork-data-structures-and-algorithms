68c3ed86a72e8666090ef6166ee7d428fae3c902
Johnathan <jocuog@gmail.com>
2022-07-16 11:53:01 -0600

    ruby selection sort recrusive benchmark

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/benchmark/benchmark.rb
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/benchmark/benchmark.rb
@@ -14,15 +14,17 @@ def selection_sort_recursive(arr)
 
 end
 
-def selection_sort_recursive(arr)
-  return [] if arr.empty?
+def selection_sort(arr)
+  sorted = []
 
-  min = arr.min
-  idx = arr.index(min)
-  arr.delete_at(idx)
-  
-  result = selection_sort_recursive(arr)
-  result.unshift(min)
+  until arr.length == 0
+    min = arr.min
+    idx = arr.index(min)
+    sorted << min
+    arr.delete_at(idx)
+  end
+
+  sorted
 end
 
 def benchmark
@@ -34,4 +36,7 @@ def benchmark
 
   (Time.now - start_time) / 1000
 end
+
+puts "iterative: #{benchmark { selection_sort([5, 8, 1, 0, -7])}}"
+puts "recursive: #{benchmark { selection_sort_recursive([5, 8, 1, 0, -7])}}"
 
