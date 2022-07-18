ad4c4e2b7313ccc4549f5d531cc8efa6b729e2d7
Johnathan <jocuog@gmail.com>
2022-07-17 21:53:21 -0600

    ruby

--- 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/ruby/balancing_parentheses.rb
+++ 03-week-3--additional-practice/00-bonus-1--balancing-parenetheses/ruby/balancing_parentheses.rb
@@ -1,5 +1,22 @@
 def balancing_parentheses(string)
   # type your code in here
+
+  parathesesO = 0
+  parathesesC = 0
+
+  string.chars.each do |char|
+    if char == '('
+      parathesesO += 1
+      next
+    end
+    
+    if parathesesO > 0 
+      parathesesO -= 1 
+    else
+      parathesesC += 1
+    end
+  end
+  parathesesC + parathesesO
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -15,6 +32,41 @@ if __FILE__ == $PROGRAM_NAME
 
   puts "Expecting: 1"
   puts balancing_parentheses(')')
+
+  puts
+
+  puts "Expecting: 0"
+  puts balancing_parentheses('()')
+
+  puts
+
+  puts "Expecting: 1"
+  puts balancing_parentheses('(')
+
+  puts
+
+  puts "Expecting: 2"
+  puts balancing_parentheses(')(')
+
+  puts
+
+  puts "Expecting: 1"
+  puts balancing_parentheses(')()')
+
+  puts
+
+  puts "Expecting: 2"
+  puts balancing_parentheses(')((((()))((())))()(()))(')
+
+  puts
+
+  puts "Expecting: 3"
+  puts balancing_parentheses(')))')
+
+  puts
+
+  puts "Expecting: 3"
+  puts balancing_parentheses('(((')
 
   # Don't forget to add your own!
 end
