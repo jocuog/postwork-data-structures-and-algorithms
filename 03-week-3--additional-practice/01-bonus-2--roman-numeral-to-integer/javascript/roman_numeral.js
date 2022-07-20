9e96d161b00d97240fd520dc07428d64f4ff93e8
Johnathan <jocuog@gmail.com>
2022-07-19 21:07:55 -0600

    roman numeral algorithm first try

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,5 +1,19 @@
 function romanNumeral(string) {
   // type your code here
+  const splitStr = string.split('');  // split the string into array
+
+  let total = 0
+  const I = 1
+  const V = 5
+  const X = 10
+  const L = 50
+  const C = 100
+  const D = 500
+  const M = 1000
+
+  splitStr.forEach(el =>  {
+    
+  })
 }
 
 if (require.main === module) {
