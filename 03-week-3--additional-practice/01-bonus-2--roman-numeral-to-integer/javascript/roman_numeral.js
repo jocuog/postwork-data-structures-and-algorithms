ab2a8216eb2e37beb7d242347a55c5551fd4813e
Johnathan <jocuog@gmail.com>
2022-07-19 21:26:17 -0600

    roman numeral solution

--- 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
+++ 03-week-3--additional-practice/01-bonus-2--roman-numeral-to-integer/javascript/roman_numeral.js
@@ -1,19 +1,43 @@
 function romanNumeral(string) {
   // type your code here
-  const splitStr = string.split('');  // split the string into array
 
-  let total = 0
-  const I = 1
-  const V = 5
-  const X = 10
-  const L = 50
-  const C = 100
-  const D = 500
-  const M = 1000
+  let total = 0   // a variable to store sum
+  let idx = 0   // variable to move the index
+  // const I = 1
+  // const V = 5
+  // const X = 10
+  // const L = 50
+  // const C = 100
+  // const D = 500
+  // const M = 1000
 
-  splitStr.forEach(el =>  {
-    
-  })
+  const romanNumeralTable = {
+    I: 1, 
+    IV:4,
+    V: 5, 
+    IX: 9,
+    X: 10, 
+    XL:40,
+    L: 50, 
+    XC:90,
+    C: 100, 
+    CD: 400,
+    D: 500, 
+    CM: 900,
+    M: 1000}  // create a hash table of roman numeral values
+
+    while (idx < string.length) {   // loop through the length of the string
+      const twoChar = string[idx] + (string[idx + 1] || '')   // create a variable that will either stroe the value of a numeral pairing or undefined
+
+      if (romanNumeralTable[twoChar] !== undefined) {
+        total += romanNumeralTable[twoChar];  // if the number is a numeral pair add the corresponding value from the hash table to the total
+        idx += 2;   // increment the index by two
+      } else { 
+        total += romanNumeralTable[string[idx]]; // else add the value from the hash table to the total
+        ++idx; // increment the index by one
+      }
+      return total;
+    }
 }
 
 if (require.main === module) {
