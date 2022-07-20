17f05855a99f5d63307bce4214aee6521b158df1
Johnathan <jocuog@gmail.com>
2022-07-20 09:40:14 -0600

    ruby rotate array

--- 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/ruby/rotate_array.rb
+++ 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/ruby/rotate_array.rb
@@ -1,5 +1,11 @@
 def rotate_array(arr, k)
   # type your code in here
+  return arr if arr.empty? || k.zero?
+
+  rotations = k % arr.length
+  removed = arr.slice!(arr.length - rotations, rotations)
+  removed + arr
+
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -18,6 +24,22 @@ if __FILE__ == $PROGRAM_NAME
   puts "Expecting: [1, 2, 3]"
   print "=> "
   print rotate_array([1, 2, 3], 3)
+
+  puts
+
+  puts "Expecting: [1, 2, 3]"
+  print rotate_array([1, 2, 3], 0)
+
+  puts
+
+  puts "Expecting: [2, 3, 1]"
+  print rotate_array([1, 2, 3], 5)
+
+  puts
+
+  puts "Expecting: []"
+  print rotate_array([], 7)
+
   # Don't forget to add your own!
 end
 
