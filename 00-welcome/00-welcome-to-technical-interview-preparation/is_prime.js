1fcff92115c2b0f6a3d9a700be17331628c41c12
Johnathan <jocuog@gmail.com>
2022-07-20 09:41:11 -0600

    check

--- 00-welcome/00-welcome-to-technical-interview-preparation/is_prime.js
+++ 00-welcome/00-welcome-to-technical-interview-preparation/is_prime.js
@@ -4,7 +4,7 @@ const isPrime = (n) => {
         return false   // since one is not considered prime we can guard against that 
     }
 
-    for (let i = 2; i < n; i++) {   // loop through starting at 2 becuase there is no need to include 1 and 0 
+    for (let i = 2; i <= Math.sqrt(n); i++) {   // loop through starting at 2 becuase there is no need to include 1 and 0. using the Math.sqrt method reduces the number of iterations becuase we dont need to run through all possible divisible numbers less than n
         if (n % i === 0) {
            // check to see if there is reamainder of any number didvied by i
         return false; 
@@ -18,22 +18,22 @@ const isPrime = (n) => {
 if (require.main === module) {
     
     console.log("Expecting: true");
-    console.log(romanNumeral(7));
+    console.log(isPrime(7));
 
     console.log("");
 
     console.log("Expecting: false");
-    console.log(romanNumeral(6));
+    console.log(isPrime(6));
 
     console.log("");
 
     console.log("Expecting: false");
-    console.log(romanNumeral(1));
+    console.log(isPrime(1));
 
     console.log("");
 
     console.log("Expecting: true");
-    console.log(romanNumeral(11));
+    console.log(isPrime(11));
 
 }
 
