32782284f4f98b0451bd892a1737b70330273971
Johnathan <jocuog@gmail.com>
2022-07-20 00:18:57 -0600

    max value of array Algo

---
+++ 00-welcome/00-welcome-to-technical-interview-preparation/max_value.js
@@ -0,0 +1,34 @@
+const maxValue = (nums) => {
+    // create a function that will return the highest value in an array
+
+    // solve wihtout using any built in arrays
+
+    let max = -Infinity // create a variable that any number will be larger than
+
+    nums.forEach(el => {
+        if (el > max) { 
+            max = el;
+        }
+    })
+    return max;
+
+}
+
+if (require.main === module) {
+
+  console.log("Expecting: 44");
+  console.log(romanNumeral([24,26,35,44]));
+
+  console.log("");
+
+  console.log("Expecting: 223");
+  console.log(romanNumeral([201,154,223,36]));
+
+  console.log("");
+
+  console.log("Expecting: 3848");
+  console.log(romanNumeral([500.3848,0,-659]))
+    
+}
+
+module.exports = maxValue;
