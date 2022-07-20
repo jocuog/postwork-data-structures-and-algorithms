10ad7302f57b7fd259e403a2c3374fed742a8863
Johnathan <jocuog@gmail.com>
2022-07-20 09:39:01 -0600

    benchmark test rotate array

--- 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
+++ 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
@@ -10,6 +10,14 @@ function rotateArray(arr, k) {
   return arr
 }
 
+function rotateArrayAlt(arr, k) {
+  const rotations = k % arr.length;
+  const removed = arr.splice(arr.length - rotations, rotations);
+
+  return removed.concat(arr);
+}
+
+
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: [4, 1, 2, 3]");
@@ -24,7 +32,37 @@ if (require.main === module) {
 
   console.log("Expecting: [1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3], 3));
+
+  console.log("");
+
+  console.log("Expecting: [1, 2, 3]");
+  console.log(rotateArray([1, 2, 3], 0));
+
+  console.log("");
+
+  console.log("Expecting: [2, 3, 1]");
+  console.log(rotateArray([1, 2, 3], 5));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log(rotateArray([], 7));
 }
+
+
+function benchmark(callback) {
+  const startTime = Date.now();
+
+  for (let i = 0; i < 1000; ++i) {
+    callback();
+  }
+
+  return (Date.now() - startTime) / 1000;
+}
+
+
+console.log("mine", benchmark(() => rotateArray([2, 3, 4, 1, 4, 56, -2, 20],6)))
+console.log("theirs", benchmark(() => rotateArrayAlt([2, 3, 4, 1, 4, 56, -2, 20],6)))
 
 module.exports = rotateArray;
 
