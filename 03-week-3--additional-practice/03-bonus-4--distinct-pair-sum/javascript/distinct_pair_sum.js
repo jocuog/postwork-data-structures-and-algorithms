a3c2006afa6310492cde0f133e5273cecc2b32ac
Johnathan <jocuog@gmail.com>
2022-07-21 22:47:49 -0600

    distinct pair sum js

--- 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
+++ 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
@@ -4,10 +4,19 @@ function distinctPairSum(arr, k) {
   // iterate through the array adding each pair and checking it against the target value
   // put any pair that meets the target that isnt already there into an array
   
-  let targetPairs = [];
+  const targetPairs = {}; // create a hash pbject to hold pairs
 
-  
+  arr.slice(0, -1).forEach((num, idx) => {  // slice out the pair the beginning of the array and iterate 
+    const nextValue = arr[idx + 1];   // create a variable to hold the next value
+    
+    if (num + nextValue === k &&  // condition to see if the number(first element) and the next value (second element)
+      targetPairs[num] === undefined && // and if num is not in the hash object
+      targetPairs[nextValue] === undefined) { // and if nextValue is not in the hash object
+      targetPairs = [num + nextValue]; // put the value pairs in the hash object
+      }
+  })
 
+  return Object.values(targetPairs)
 }
 
 if (require.main === module) {
