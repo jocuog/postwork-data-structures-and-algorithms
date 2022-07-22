01b8b948ca1705ee4c7060a206dbd1cc481590df
Johnathan <jocuog@gmail.com>
2022-07-21 22:58:00 -0600

    alternate solution attempt

--- 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
+++ 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
@@ -4,19 +4,34 @@ function distinctPairSum(arr, k) {
   // iterate through the array adding each pair and checking it against the target value
   // put any pair that meets the target that isnt already there into an array
   
-  const targetPairs = {}; // create a hash pbject to hold pairs
+  // const targetPairs = {}; // create a hash pbject to hold pairs
 
-  arr.slice(0, -1).forEach((num, idx) => {  // slice out the pair the beginning of the array and iterate 
-    const nextValue = arr[idx + 1];   // create a variable to hold the next value
+  // arr.slice(0, -1).forEach((num, idx) => {  // slice out the pair the beginning of the array and iterate 
+  //   const nextValue = arr[idx + 1];   // create a variable to hold the next value
     
-    if (num + nextValue === k &&  // condition to see if the number(first element) and the next value (second element)
-      targetPairs[num] === undefined && // and if num is not in the hash object
-      targetPairs[nextValue] === undefined) { // and if nextValue is not in the hash object
-      targetPairs = [num + nextValue]; // put the value pairs in the hash object
-      }
-  })
+  //   if (num + nextValue === k &&  // condition to see if the number(first element) and the next value (second element)
+  //     targetPairs[num] === undefined && // and if num is not in the hash object
+  //     targetPairs[nextValue] === undefined) { // and if nextValue is not in the hash object
+  //     targetPairs[num] = [num + nextValue]; // put the value pairs in the hash object
+  //     }
+  // })
 
-  return Object.values(targetPairs)
+  // return Object.values(targetPairs)
+
+    const pairs = {};
+
+  arr.slice(0, -1).forEach((num, idx) => {
+    const nextValue = arr[idx + 1];
+
+    if (num + nextValue === k && 
+      pairs[num] === undefined && 
+      pairs[nextValue] === undefined) {
+        pairs[num] = [num, nextValue];
+    }
+  });
+
+  return Object.values(pairs);
+
 }
 
 if (require.main === module) {
@@ -28,6 +43,31 @@ if (require.main === module) {
 
   console.log("Expecting: [[2, 8]]");
   console.log("=>", distinctPairSum([3, 4, 2, 1, 5, 2, 8, 2], 10));
+  
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log(distinctPairSum([3, 4, 2, 1, 5, 2, 8, 2], 100));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log(distinctPairSum([], 100));
+
+  console.log("");
+
+  console.log("Expecting: [[59, 41]]");
+  console.log(distinctPairSum([59, 41], 100));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log(distinctPairSum([59], 100));
+
+  console.log("");
+
+  console.log("Expecting: [[0, 0], [10, -10], [3, -3]]");
+  console.log(distinctPairSum([1, 0, 0, 10, -10, 5, 4, 3, -3, -3], 0));
 }
 
 module.exports = distinctPairSum;
