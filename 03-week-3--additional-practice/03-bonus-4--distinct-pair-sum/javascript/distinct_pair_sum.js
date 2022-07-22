b31fd08f79c5f27b0dc27e93b63984b12c3f2951
Johnathan <jocuog@gmail.com>
2022-07-21 23:02:59 -0600

    fixed distinct pair sum algo

--- 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
+++ 03-week-3--additional-practice/03-bonus-4--distinct-pair-sum/javascript/distinct_pair_sum.js
@@ -4,34 +4,19 @@ function distinctPairSum(arr, k) {
   // iterate through the array adding each pair and checking it against the target value
   // put any pair that meets the target that isnt already there into an array
   
-  // const targetPairs = {}; // create a hash pbject to hold pairs
+  const targetPairs = {}; // create a hash pbject to hold pairs
 
-  // arr.slice(0, -1).forEach((num, idx) => {  // slice out the pair the beginning of the array and iterate 
-  //   const nextValue = arr[idx + 1];   // create a variable to hold the next value
+  arr.slice(0, -1).forEach((num, idx) => {  // slice out the pair the beginning of the array and iterate 
+    const nextValue = arr[idx + 1];   // create a variable to hold the next value
     
-  //   if (num + nextValue === k &&  // condition to see if the number(first element) and the next value (second element)
-  //     targetPairs[num] === undefined && // and if num is not in the hash object
-  //     targetPairs[nextValue] === undefined) { // and if nextValue is not in the hash object
-  //     targetPairs[num] = [num + nextValue]; // put the value pairs in the hash object
-  //     }
-  // })
+    if (num + nextValue === k &&  // condition to see if the number(first element) and the next value (second element)
+      targetPairs[num] === undefined && // and if num is not in the hash object
+      targetPairs[nextValue] === undefined) { // and if nextValue is not in the hash object
+      targetPairs[num] = [num, nextValue]; // put the value pairs in the hash object
+      }
+  })
 
-  // return Object.values(targetPairs)
-
-    const pairs = {};
-
-  arr.slice(0, -1).forEach((num, idx) => {
-    const nextValue = arr[idx + 1];
-
-    if (num + nextValue === k && 
-      pairs[num] === undefined && 
-      pairs[nextValue] === undefined) {
-        pairs[num] = [num, nextValue];
-    }
-  });
-
-  return Object.values(pairs);
-
+  return Object.values(targetPairs)
 }
 
 if (require.main === module) {
